607684127ff272365c93984e3ab21b29cc6176ce
Kirikiri2 <worklisa271@gmail.com>
2024-04-20 00:00:44 +0300

    Добавила карусель.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Flickity from 'react-flickity-component';
+import './flickity.css'
+import cloud from "./images/cloud.jpg"
+import tree from "./images/tree.jpg"
+import tree2 from "./images/tree2.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const flickity0ptions =
+{
+initialIndex: 2
 }
 
+const cars = {
+id: [1,2,3],
+title: ['tree', "tree2", 'cloud'],
+image: [cloud, tree, tree2]
+}
+
+function App() {
+    return (
+        <div className= 'App'>
+            <Flickity
+            className= "Slider"
+            elementType= "div"
+            disableImagesLoaded= {false}
+            options={flickity0ptions}
+            reloadOnUpdate
+            static
+            >
+            {cars['id'].map((index) =>{
+                return (
+                    <div key={index} className='Plate'>
+                        <> 
+                        <h2>{cars['title'][index - 1]}</h2>
+                        <div style={{
+                            backgroundImage: `url(${cars['image'][index - 1]})`,
+                            width: "100%",
+                            height: 600,
+                            backgroundSize: "cover",
+                            }}></div>
+                        </>
+                    </div>
+                ) 
+            })}
+
+            </Flickity>
+        </div>
+    
+    )
+
+}
 export default App;
