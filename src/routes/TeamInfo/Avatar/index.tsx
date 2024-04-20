ac860d5a30503e5f2da8d778d140c0a969937cc7
f4cax <g53705046@gmail.com>
2024-04-21 00:54:40 +0300

    добавил аватар

---
+++ src/routes/TeamInfo/Avatar/index.tsx
@@ -0,0 +1,12 @@
+import './ava.css';
+import React from 'react';
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img src="avatar.jpg" alt="Avatar" />
+    </div>
+  );
+}
+
+export default Avatar;
--- src/routes/TeamInfo/index.tsx
+++ src/routes/TeamInfo/index.tsx
@@ -1,13 +1,16 @@
 import './TeamInfo.css';
 import SignUpForm from './SignUpForm';
+import Avatar from './Avatar';
 
 export default function TeamInfo() {
 	return (
 		<div id="TeamInfo">
 			<h2>TODO</h2>
 			<SignUpForm />
+			<Avatar />
 			<img src='/images/image.jpg' alt='lol' />
 			<img src='/images/skull.png' alt='skull' />
 		</div>
+		
 	);
 }
